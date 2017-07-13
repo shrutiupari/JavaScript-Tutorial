309d0f2c574f790f059658f9c1d7a7cf09f168d6
Shruti <shrutiupari@users.noreply.github.com>
2017-07-13 18:36:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<html>
+    <head>
+        <title>JavaScript Tutorial</title>
+      <link rel ="stylesheet" style="style/css" href="css/style.css"/>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+<link href='http://fonts.which is googleapis.com/css?family=Roboto:400,300,300italic,500,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//storage.googleapis.com/code.getmdl.io/1.0.1/material.teal-red.min.css" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="/lib/w3-theme-amber.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.js"></script>
+      </head>
+      <body>
+<div class="mdl-layout mdl-js-layout">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">JavaScript Tutorial</span>
+      <div class="mdl-layout-spacer"></div>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="#">Home</a>
+        <a class="mdl-navigation__link" href="#">About</a>
+        <a class="mdl-navigation__link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
+   
+<div class="w3-row-padding w3-theme">
+
+<div class="w3-third w3-section">
+<div class="w3-card-4">
+
+<div class="w3-container w3-red">
+<h4>Cinque Terre</h4>
+<p>The Cinque Terre (five lands) is a portion of the Italian Riviera. The coastline with five villages: Monterosso, Vernazza, Corniglia, Manarola, and Riomaggiore
+is a UNESCO World Heritage Site.</p>
+</div>
+</div>
+</div>
+</div>
+       
+
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">JS Tutorial</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="index.php">JS Home</a>
+      <a class="mdl-navigation__link" href="#">JS Introduction</a>
+      <a class="mdl-navigation__link" href="#">JS Where to</a>
+      <a class="mdl-navigation__link" href="#">JS Output</a>
+      <a class="mdl-navigation__link" href="#">JS Statements</a>
+      <a class="mdl-navigation__link" href="#">JS Syntax</a>
+      <a class="mdl-navigation__link" href="#">JS Comments</a>
+      <a class="mdl-navigation__link" href="#">JS Variables</a>
+      <a class="mdl-navigation__link" href="#">JS Operators</a>
+      <a class="mdl-navigation__link" href="#">JS Arithmetic</a>
+      <a class="mdl-navigation__link" href="#">JS Assignments</a>
+      <a class="mdl-navigation__link" href="#">JS Data Types</a>
+      <a class="mdl-navigation__link" href="#">JS Functions</a>
+      <a class="mdl-navigation__link" href="#">JS Objects</a>
+      <a class="mdl-navigation__link" href="#">JS Scope</a>
+      <a class="mdl-navigation__link" href="#">JS Events</a>
+      <a class="mdl-navigation__link" href="#">JS String</a>
+      <a class="mdl-navigation__link" href="#">JS String Methods</a>
+      <a class="mdl-navigation__link" href="#">JS Numbers</a>
+      <a class="mdl-navigation__link" href="#">JS Number Methods</a>
+      <a class="mdl-navigation__link" href="#">JS Math</a>
+      <a class="mdl-navigation__link" href="#">JS Random</a>
+      <a class="mdl-navigation__link" href="#">JS Date</a>
+      <a class="mdl-navigation__link" href="#">JS Date Formats</a>
+      <a class="mdl-navigation__link" href="#">JS Date Methods</a>
+      <a class="mdl-navigation__link" href="#">JS Array</a>
+      <a class="mdl-navigation__link" href="#">JS Array methods</a>
+      <a class="mdl-navigation__link" href="#">JS Array Sort</a>
+      <a class="mdl-navigation__link" href="#">JS Booleans</a>
+      <a class="mdl-navigation__link" href="#">JS Comparisons</a>
+      <a class="mdl-navigation__link" href="#">JS Conditions</a>
+      <a class="mdl-navigation__link" href="#">JS Switch</a>
+      <a class="mdl-navigation__link" href="#">JS Loop For</a>
+      <a class="mdl-navigation__link" href="#">JS Loop While</a>
+      <a class="mdl-navigation__link" href="#">JS Break</a>
+      <a class="mdl-navigation__link" href="#">JS Type Conversion</a>
+      <a class="mdl-navigation__link" href="#">JS Forms</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX Introduction</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX XMLHttp</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX Request</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX Response</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX XML File</a>
+      <a class="mdl-navigation__link" href="#">JS AJAX PHP</a>
+      <a class="mdl-navigation__link" href="#">JS Function Definition</a>
+      <a class="mdl-navigation__link" href="#">JS Function parameters</a>
+      <a class="mdl-navigation__link" href="#">JS Object Definition</a>
+      <a class="mdl-navigation__link" href="#">JS Object Properties</a>
+      <a class="mdl-navigation__link" href="#">JS Object Methods</a>
+      <a class="mdl-navigation__link" href="#">JS Object Prototypes</a>
+      <a class="mdl-navigation__link" href="#">JS Cookies</a>
+    </nav>
+  </div>
+  </div>
+</div>
+</body>
+    </html>
